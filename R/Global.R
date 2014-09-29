992d342798c8f5151077b77c75851237a6f129b7
mcanouil <mickael.canouil@yahoo.fr>
2014-09-29 08:47:51 +0200

    Version 1.5.1
    Signed-off-by: Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>

--- R/Global.R
+++ R/Global.R
@@ -53,15 +53,26 @@ GC <- function (verbose = getOption("verbose"), reset = FALSE) {
 
 maxCores <- function (mc.cores = 1) {
     if (Sys.info()[["sysname"]] != "Windows") {
+        nbCores <- detectCores()
         mc.cores.old <- mc.cores
-        sysMemFree <- system("egrep '^MemFree:' /proc/meminfo", intern = TRUE)
-        sysMemCached <- system("egrep '^Cached:' /proc/meminfo", intern = TRUE)
-        sysMemAvailable <- 0.95*(as.numeric(gsub("[^0-9]*([0-9]*)", "\\1", sysMemFree)) + as.numeric(gsub("[^0-9]*([0-9]*)", "\\1", sysMemCached)))
-        sysProc <- as.numeric(unlist(strsplit(system(paste("ps v", Sys.getpid()), intern = TRUE)[2], " +"))[8])
-        mc.cores <- max(min(as.numeric(mc.cores), floor(sysMemAvailable/sysProc)), 1)
-        if (mc.cores != mc.cores.old) {
-            warning(paste0('[mclapply2] To avoid memory overload "mc.cores" was decreased to "', mc.cores, '".'), call. = FALSE)
-        } else {}
+            if (file.exists("/proc/meminfo")) {
+            memInfo <- readLines("/proc/meminfo")
+            # sysMemFree <- system("egrep '^MemFree:' /proc/meminfo", intern = TRUE)
+            sysMemFree <- memInfo[grep('^MemFree:', memInfo)]
+            # sysMemCached <- system("egrep '^Cached:' /proc/meminfo", intern = TRUE)
+            sysMemCached <- memInfo[grep('^Cached:', memInfo)]
+            sysMemAvailable <- 0.95*(as.numeric(gsub("[^0-9]*([0-9]*)", "\\1", sysMemFree)) + as.numeric(gsub("[^0-9]*([0-9]*)", "\\1", sysMemCached)))
+            sysProc <- as.numeric(unlist(strsplit(system(paste("ps v", Sys.getpid()), intern = TRUE)[2], " +"))[8])
+            mc.cores <- max(min(as.numeric(mc.cores), floor(sysMemAvailable/sysProc)), 1)
+            if (mc.cores > nbCores) {
+                mc.cores <- nbCores
+            } else {}
+            if (mc.cores != mc.cores.old) {
+                warning(paste0('To avoid memory overload "mc.cores" was decreased to "', mc.cores, '".'), call. = FALSE)
+            } else {}
+        } else {
+            mc.cores <- ifelse(mc.cores.old>nbCores, nbCores, mc.cores.old)
+        }
     } else {}
     return(mc.cores)
 }
