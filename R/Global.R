d89c44e8d58a381b8e74a80af12dd545c0460092
mcanouil <mickael.canouil@yahoo.fr>
2014-11-03 11:12:49 +0100

    Version 1.5.10

--- R/Global.R
+++ R/Global.R
@@ -260,7 +260,8 @@ writeLD <- function (pattern = "Chrom", snpInfoDir, signalFile, ldDir = NULL, ld
 
 
 .readSNP <- function (pattern, snpListDir) {
-    snpListFile <- grep(pattern, sort(list.files(snpListDir, full.names = TRUE)), value = TRUE)[1]
+    # snpListFile <- grep(pattern, sort(list.files(snpListDir, full.names = TRUE)), value = TRUE)[1]
+    snpListFile <- list.files(gsub("/$", "", snpListDir), pattern = paste0(pattern, "[^0-9].*.txt"), full.names = TRUE)
     if (is.na(snpListFile)) {
         snpList <- data.frame()
     } else {
@@ -351,10 +352,10 @@ writeLD <- function (pattern = "Chrom", snpInfoDir, signalFile, ldDir = NULL, ld
 
 
 .readFiles <- function (pattern, snpInfoDir, snpListDir, distThresh) {
-    newPattern <- unlist(strsplit(grep(paste0(pattern, "[^0-9]*.bim"), sort(list.files(snpInfoDir)), value = TRUE), ".bim"))[1]
-    eSNP <- .readSNP(pattern = newPattern, snpListDir = snpListDir)
-    signal <- .readSignal(pattern = newPattern)
-    plinkData <- .readFreq(pattern = newPattern, snpInfoDir = snpInfoDir)
+    # newPattern <- unlist(strsplit(grep(paste0(pattern, "[^0-9]*.bim"), sort(list.files(snpInfoDir)), value = TRUE), ".bim"))[1]
+    eSNP <- .readSNP(pattern = pattern, snpListDir = snpListDir)
+    signal <- .readSignal(pattern = pattern)
+    plinkData <- .readFreq(pattern = pattern, snpInfoDir = snpInfoDir)
 
     signalPlink <- merge(signal, plinkData, by = "SNP")
     if (nrow(eSNP)!=0) {
