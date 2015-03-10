aaa188a2ba84d6d9280c0d331bb7fd5fbcbb6029
mcanouil <mickael.canouil@cnrs.fr>
2015-03-10 16:43:09 +0100

    Version 1.6.4

--- R/enrichment.R
+++ R/enrichment.R
@@ -1273,7 +1273,17 @@ setMethod(f = "getEnrichSNP", signature = "Enrichment", definition = function (o
         },
         "xSNP" = {
             if (object["Call"][["readEnrichment"]][["LD"]]) {
-                object["Data"][object["Data"][, "PVALUE"]<alpha & object["Data"][, type]==1, ]
+                message('Loading ...')
+                dataSNP <- object["Data"]
+                dataLD <- object["LD"]
+                xSNP <- dataSNP[dataSNP[, "PVALUE"]<alpha & dataSNP[, type]==1, ]
+                dataLDtmp <- dataLD[dataLD %in% xSNP[, "SNP"]]
+                dataLDtmp <- cbind(SNP_A = names(dataLDtmp), SNP_B = dataLDtmp)
+                dataLDtmp <- dataLDtmp[dataLDtmp[, "SNP_A"]%in%dataSNP[dataSNP[, "eSNP"]%in%1, "SNP"], ]
+                xSNPld <- do.call("rbind", by(dataLDtmp, dataLDtmp[, "SNP_B"], function (iDta) {
+                    cbind(xSNP = unique(iDta[, "SNP_B"]), LD_with_eSNP = paste(iDta[, "SNP_A"], collapse = ";"))
+                }))
+                merge(xSNP, xSNPld, by.x = "SNP", by.y = "xSNP")
             } else {
                 warning('[Enrichment:getEnrichSNP] significant "eSNP" are returned instead of "xSNP",\n    "readEnrichment" should be run with "LD=TRUE".', call. = FALSE)
                 object["Data"][object["Data"][, "PVALUE"]<alpha & object["Data"][, type]==1, ]
